90d898105dbcbb76390c63407c5e0b6bce0a12d3
YueErro <yue.erro@pal-robotics.com>
2023-05-16 11:06:03 +0200

    omnibase gazebo to ROS 2: + colcon + launch.py

---
+++ omni_base_gazebo/launch/omni_base_gazebo.launch.py
@@ -0,0 +1,79 @@
+# Copyright (c) 2023 PAL Robotics S.L. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from os import environ, pathsep
+
+from ament_index_python.packages import get_package_prefix, get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+# from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+# from launch.substitutions import LaunchConfiguration
+
+from launch_pal.include_utils import include_launch_py_description
+
+
+def generate_launch_description():
+    world_name_arg = DeclareLaunchArgument(
+        'world_name', default_value='pal_office',
+        description="Specify world name, we'll convert to full path"
+    )
+
+    navigation_arg = DeclareLaunchArgument(
+        'navigation', default_value='false',
+        description='Specify if launching Navigation2'
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('pal_gazebo_worlds'),
+            'launch'), '/pal_gazebo.launch.py']),
+    )
+
+    omni_base_spawn = include_launch_py_description(
+        'omni_base_gazebo', ['launch', 'omni_base_spawn.launch.py'])
+    omni_base_bringup = include_launch_py_description(
+        'omni_base_bringup', ['launch', 'omni_base_bringup.launch.py'])
+
+    # navigation = include_launch_py_description(
+    #     'omni_base_2dnav', ['launch', 'omni_base_nav_bringup.launch.py'],
+    #     condition=IfCondition(LaunchConfiguration('navigation')))
+
+    pkg_path = get_package_prefix('omni_base_description')
+    model_path = os.path.join(pkg_path, 'share')
+    resource_path = pkg_path
+
+    if 'GAZEBO_MODEL_PATH' in environ:
+        model_path += pathsep + environ['GAZEBO_MODEL_PATH']
+    if 'GAZEBO_RESOURCE_PATH' in environ:
+        resource_path += pathsep + environ['GAZEBO_RESOURCE_PATH']
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    ld.add_action(SetEnvironmentVariable('GAZEBO_MODEL_PATH', model_path))
+    # Using this prevents shared library from being found
+    # ld.add_action(SetEnvironmentVariable('GAZEBO_RESOURCE_PATH', omni_base_resource_path))
+
+    ld.add_action(world_name_arg)
+    ld.add_action(gazebo)
+    ld.add_action(omni_base_spawn)
+    ld.add_action(omni_base_bringup)
+
+    ld.add_action(navigation_arg)
+    # ld.add_action(navigation)
+
+    return ld
---
+++ omni_base_gazebo/launch/omni_base_spawn.launch.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2023 PAL Robotics S.L. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # This format doesn't work because because we have to expand gzpose into
+    # different args for spawn_entity.py
+    # gz_pose = DeclareLaunchArgument(
+    #     'gzpose', default_value='-x 0 -y 0 -z 0.0 -R 0.0 -P 0.0 -Y 0.0 ',
+    #     description='Spawn gazebo position as provided to spawn_entity.py'
+    # )
+    model_name = DeclareLaunchArgument(
+        'model_name', default_value='omni_base',
+        description='Gazebo model name'
+    )
+
+    spawn_entity = Node(package='gazebo_ros',
+                        executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description', '-entity',
+                                   LaunchConfiguration('model_name'),
+                                   # LaunchConfiguration('gzpose'),
+                                   ],
+                        output='screen')
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # ld.add_action(gz_pose)
+    ld.add_action(model_name)
+    ld.add_action(spawn_entity)
+
+    return ld
