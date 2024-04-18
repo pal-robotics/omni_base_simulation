c5cc38d580f8d383aadf0aa720580c3e1847a4d3
martinaannicelli <martina.annicelli@pal-robotics.com>
2024-04-18 18:13:13 +0200

    added pose variables in spawn entity

--- omni_base_gazebo/launch/omni_base_spawn.launch.py
+++ omni_base_gazebo/launch/omni_base_spawn.launch.py
@@ -19,14 +19,19 @@ from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
     # This format doesn't work because because we have to expand gzpose into
     # different args for spawn_entity.py
-    # gzpose = DeclareLaunchArgument(
-    #    'gzpose', default_value='-x 0 -y 0 -z 0.0 -R 0.0 -P 0.0 -Y 0.0 ',
-    #    description='Spawn gazebo position as provided to spawn_entity.py'
-    # )
+    
+    pose = {
+        'x': LaunchConfiguration('x', default='0.0'),
+        'y': LaunchConfiguration('y', default='0.0'),
+        'z': LaunchConfiguration('z', default='0.0'),
+        'R': LaunchConfiguration('R', default='0.0'),
+        'P': LaunchConfiguration('P', default='0.0'),
+        'Y': LaunchConfiguration('Y', default='0.0'),
+    }
+    
     model_name = DeclareLaunchArgument(
         'model_name', default_value='omni_base',
         description='Gazebo model name'
@@ -36,21 +41,25 @@ def generate_launch_description():
                         executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description', '-entity',
                                    LaunchConfiguration('model_name'),
-                                   # LaunchConfiguration('gzpose'),
-                                   '-x', '0.0',
-                                   '-y', '0.0',
-                                   '-z', '0.0',
-                                   '-R', '0.0',
-                                   '-P', '0.0',
-                                   '-Y', '0.0'
+                                   '-x',
+                                    pose['x'],
+                                    '-y',
+                                    pose['y'],
+                                    '-z',
+                                    pose['z'],
+                                    '-R',
+                                    pose['R'],
+                                    '-P',
+                                    pose['P'],
+                                    '-Y',
+                                    pose['Y'],
                                    ],
                         output='screen')
 
     # Create the launch description and populate
     ld = LaunchDescription()
-
-    # ld.add_action(gz_pose)
+    
     ld.add_action(model_name)
     ld.add_action(spawn_entity)
-
+    
     return ld
