8f168ed8a2764c3e28595211b2f9489eb9013d60
David ter Kuile <david.terkuile@pal-robotics.com>
2024-06-27 13:03:28 +0200

    Change rgbd sensors to add-on-module

--- omni_base_gazebo/launch/omni_base_gazebo.launch.py
+++ omni_base_gazebo/launch/omni_base_gazebo.launch.py
@@ -33,7 +33,7 @@ from omni_base_description.launch_arguments import OmniBaseArgs
 class LaunchArguments(LaunchArgumentsBase):
     wheel_model: DeclareLaunchArgument = OmniBaseArgs.wheel_model
     laser_model: DeclareLaunchArgument = OmniBaseArgs.laser_model
-    rgbd_sensors: DeclareLaunchArgument = OmniBaseArgs.rgbd_sensors
+    add_on_module: DeclareLaunchArgument = OmniBaseArgs.add_on_module
     is_public_sim: DeclareLaunchArgument = CommonArgs.is_public_sim
     world_name: DeclareLaunchArgument = CommonArgs.world_name
     navigation: DeclareLaunchArgument = CommonArgs.navigation
@@ -118,7 +118,7 @@ def declare_actions(
         launch_arguments={
             'wheel_model': launch_args.wheel_model,
             'laser_model': launch_args.laser_model,
-            'rgbd_sensors': launch_args.rgbd_sensors,
+            'add_on_module': launch_args.add_on_module,
             'use_sim_time': LaunchConfiguration('use_sim_time'),
             'is_public_sim': launch_args.is_public_sim,
 
